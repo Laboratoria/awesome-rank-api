953d7bbf7b28b544106e94049d6df828d20000cd
Ivan Medina <medinadiazivan@gmail.com>
2016-12-03 14:39:43 -0500

    Set Access-Control-Allow-Origin *

--- server.js
+++ server.js
@@ -74,6 +74,25 @@ apiRoutes.get('/developers', function(req, res) {
 
 app.use('/api', apiRoutes);
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.listen(app.get('port'), function () {
 	console.log('Server started on port ' + app.get('port'));
 })
