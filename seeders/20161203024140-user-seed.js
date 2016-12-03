4e9f429d71beee7ae2655053a9e76ab87e10ed94
Ivan Medina <medinadiazivan@gmail.com>
2016-12-03 11:02:10 -0500

    Heroku API Test

---
+++ seeders/20161203024140-user-seed.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var crypto = require('crypto'),
+    algorithm = 'aes-256-ctr',
+    password = 'laboraotria';
+
+function encrypt(text){
+  var cipher = crypto.createCipher(algorithm,password)
+  var crypted = cipher.update(text,'utf8','hex')
+  crypted += cipher.final('hex');
+  return crypted;
+}
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Users', [{
+      name: 'Ivan',
+      lastname: 'Medina',
+      username: 'ivandevp',
+      password: encrypt('laboratoria'),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
---
+++ server.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var jwt = require('jsonwebtoken');
+var crypto = require('crypto'),
+    algorithm = 'aes-256-ctr',
+    password = 'laboraotria';
+
+var models = require('./models');
+
+app.set('port', process.env.PORT || 8080);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(morgan('dev'));
+
+var apiRoutes = express.Router(); 
+
+var apiRoutes = express.Router(); 
+
+var encrypt = function (text){
+  var cipher = crypto.createCipher(algorithm,password)
+  var crypted = cipher.update(text,'utf8','hex')
+  crypted += cipher.final('hex');
+  return crypted;
+};
+ 
+var decrypt = function (text){
+  var decipher = crypto.createDecipher(algorithm,password)
+  var dec = decipher.update(text,'hex','utf8')
+  dec += decipher.final('utf8');
+  return dec;
+};
+
+app.get('/', function(req, res) {
+    res.send('Hello! The API is at http://localhost:' + app.get('port') + '/api');
+});
+
+apiRoutes.post('/login', function(req, res) {
+    var auth = {
+    	username: req.body.username,
+    	password: req.body.password
+    };
+
+    models.User.find({ 
+    	where: { 
+    		username: auth.username, 
+    		password: encrypt(auth.password)
+    	} 
+    }).then(function(user) {
+		if (!user) {
+			console.log('No user with the username ' + auth.username + ' has been found.');
+			res.json({ success: false });
+		} else {
+			console.log('Hello ' + auth.username + '!');
+			console.log('All attributes of user:', user.get());
+			res.json({ success: true, user: auth });
+		}
+	});
+});
+
+app.use('/api', apiRoutes);
+
+app.listen(app.get('port'), function () {
+	console.log('Server started on port ' + app.get('port'));
+})
