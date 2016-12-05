456f2d8870e8bdc0689c0e07145898f144e93fd2
Ivan Medina <medinadiazivan@gmail.com>
2016-12-05 07:30:57 -0500

    User seeds

--- seeders/20161203024140-user-seed.js
+++ seeders/20161203024140-user-seed.js
@@ -21,6 +21,86 @@ module.exports = {
       password: encrypt('laboratoria'),
       createdAt: new Date(),
       updatedAt: new Date()
+    }, {
+      name: 'Banco',
+      lastname: 'Credito',
+      username: 'bcp',
+      company: 'BCP',
+      password: encrypt('bcp'),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      name: 'Empresa',
+      lastname: 'GMD',
+      username: 'gmd',
+      company: 'GMD',
+      password: encrypt('gmd'),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      name: 'Empresa',
+      lastname: 'Tekton Labs',
+      username: 'tekton',
+      company: 'Tekton',
+      password: encrypt('tekton'),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      name: 'Ministerio',
+      lastname: 'Produccion',
+      username: 'minproduccion',
+      company: 'MinProduccion',
+      password: encrypt('minproduccion'),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      name: 'Empresa',
+      lastname: 'Thoughtworks',
+      username: 'thoughtworks',
+      company: 'Thoughtworks',
+      password: encrypt('thoughtworks'),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      name: 'Banco',
+      lastname: 'Scotiabank',
+      username: 'scotiabank',
+      company: 'Scotiabank',
+      password: encrypt('scotiabank'),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      name: 'Empresa',
+      lastname: 'Urbaner',
+      username: 'urbaner',
+      company: 'Urbaner',
+      password: encrypt('urbaner'),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      name: 'Empresa',
+      lastname: 'Lyft',
+      username: 'lyft',
+      company: 'Lyft',
+      password: encrypt('lyft'),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      name: 'Empresa',
+      lastname: 'Globant',
+      username: 'globant',
+      company: 'Globant',
+      password: encrypt('globant'),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      name: 'Empresa',
+      lastname: 'Cignum',
+      username: 'cignum',
+      company: 'Cignum',
+      password: encrypt('cignum'),
+      createdAt: new Date(),
+      updatedAt: new Date()
     }], {});
   },
 
