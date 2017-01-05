6e83304cc5da44ed02b3e556c5710f57d5f7275a
Iván Medina Díaz <medinadiazivan@gmail.com>
2017-01-05 17:13:51 +0000

    Set foreign key checks to 0

--- migrations/20170104213243-add-squad-id-to-developer.js
+++ migrations/20170104213243-add-squad-id-to-developer.js
@@ -2,15 +2,20 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    return queryInterface.addColumn('developers', 'squadId', {
-      type: Sequelize.INTEGER,
-      allowNull: Sequelize,
-      defaultValue: 0,
-      references: {
-        model: 'squads',
-        key: 'id'
-      }
-    });
+    var options = { raw: true };
+
+    return queryInterface.sequelize.query('SET FOREIGN_KEY_CHECKS = 0', options)
+      .then(function () {
+        queryInterface.addColumn('developers', 'squadId', {
+          type: Sequelize.INTEGER,
+          allowNull: Sequelize,
+          defaultValue: 0,
+          references: {
+            model: 'squads',
+            key: 'id'
+          }
+        });
+      });
   },
 
   down: function (queryInterface, Sequelize) {
