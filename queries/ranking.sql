7642efe180f4a811213302dfcd91695af072c943
Ivan Medina <medinadiazivan@gmail.com>
2017-01-12 17:38:29 -0500

    Order results

--- queries/ranking.sql
+++ queries/ranking.sql
@@ -27,4 +27,4 @@ inner join squads s on d.squadId = s.id
 inner join tech_points tp on tp.id = d.id
 inner join soft_skill_points tss on tss.id = d.id
 group by d.name, d.lastname, d.age, d.campus, d.photoUrl 
-order by (tp.avgtech + tss.avghse);
+order by (tp.avgtech + tss.avghse) desc;
